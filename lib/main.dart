fe145349ffdaabeeefa7603889e98e4e62165b16
ibmnandkishor <nand86363@gmail.com>
2024-05-08 15:56:17 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ToDo List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ToDoList(),
+    );
+  }
+}
+
+class ToDoList extends StatefulWidget {
+  @override
+  _ToDoListState createState() => _ToDoListState();
+}
+
+class _ToDoListState extends State<ToDoList> {
+  List<String> tasks = [];
+
+  TextEditingController _taskController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: Text('ToDo List',
+        style: TextStyle(color: Colors.white),        
+        ),
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(8.0),
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(tasks[index],
+            style: TextStyle(color: Colors.red, fontSize: 30),
+            
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                setState(() {
+                  tasks.removeAt(index);
+                });
+              },
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showAddTaskDialog(context);
+        },
+        // child: Icon(Icons.add),
+        child:Text('Add List'),
+      ),
+    );
+  }
+
+  void _showAddTaskDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Add Task'),
+          content: TextField(
+            controller: _taskController,
+            decoration: InputDecoration(hintText: 'Enter task'),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  tasks.add(_taskController.text);
+                  _taskController.clear();
+                  Navigator.pop(context);
+                });
+              },
+              child: Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
